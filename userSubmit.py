0fb7e48902eb4eae8af95dfede28caa94d793cf1
BATOER-0605 <nem0605@outlook.com>
2024-05-16 17:40:05 +0900

    ver1.0

--- userSubmit.py
+++ userSubmit.py
@@ -5,6 +5,6 @@ csvfile = open("userList.csv","r",encoding="utf-8")
 file = csv.reader(csvfile, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 
 for row in file:
-    dbc.create_new_user(display_name=row[2],name=row[3][:7],email=row[3],isAdmin=False,passwd=row[3][:7],grade=row[0],user_class=row[1],discord='未設定')
+    dbc.create_new_user(display_name=row[2],name=row[3][:7],email=row[3][:7]+'@edu.asahikawa-nct.ac.jp',isAdmin=False,passwd=row[3][:7],grade=row[0],user_class=row[1],discord='未設定')
 
 csvfile.close()
