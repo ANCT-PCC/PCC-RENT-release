13c273b6f220ab068c483549505e02eb8c72b0bf
BATOER-0605 <nem0605@outlook.com>
2024-05-16 18:00:19 +0900

    ver.1.0

--- grade_updater.py
+++ grade_updater.py
@@ -4,5 +4,5 @@ allusers = dbc.get_all_users()
 
 for row in allusers:
     prev_grade = int(row[9][:1])
-    new_grade = prev_grade+1
+    new_grade = prev_grade+1+'年 '
     dbc.update_user_info(row[1],'grade',str(new_grade))
