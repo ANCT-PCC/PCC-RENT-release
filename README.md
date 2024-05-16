1b86189f48b35fe29bcf0f39a643c2af8ca5c5dc
BATOER-0605 <nem0605@outlook.com>
2024-05-16 17:20:10 +0900

    いろいろ

--- README.md
+++ README.md
@@ -19,4 +19,13 @@ CLIでのログインスクリプト
 MacBook Air 13 (Apple M2)  
 macOS Sonoma 14  
 Python 3.10.11  
-モジュール: req.txt参照
+モジュール: req.txt参照  
+
+## インストール  
+python3.10 -m venv .venv  
+source ./.venv/bin/activate  
+pip install -r req.txt  
+userSubmit.csvとitemSubmit.csvに内容を記述してから  
+./userSubmit.sh  
+./itemSubmit.sh  
+./start.sh
--- test.py
+++
@@ -1,7 +0,0 @@
-import dbc
-
-dbc.create_new_user("齋藤 直人","s203120","s203120@edu.asahikawa-nct.ac.jp",False,"test",5,31,"Not Set")
-dbc.create_new_user("齋藤 直人","naoto","s203120@edu.asahikawa-nct.ac.jp",True,"test",5,31,"Not Set")
-
-dbc.create_new_item("00","1億円","銀行強盗で入手","金庫","なし","NoPic")
-dbc.create_new_item("1","10億円","宝くじで当たった","金庫","なし","NoPic")
