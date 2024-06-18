89b214945bb8ccdce14b1488111f39d25c1d275a
BATOER-0605 <nem0605@outlook.com>
2024-06-18 17:51:17 +0900

    chane webhook

--- send_discord.py
+++ send_discord.py
@@ -8,7 +8,7 @@ uname = sys.argv[2]
 
 async def foo(msg,uname):
     async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url('https://discord.com/api/webhooks/1240620878597984337/iYfPWx7X-s4u-dFBNVvftOlwXYU0Jrugtu839wtYwuCc2hjpDGAHUMe161tXzoCnjMLP', session=session)
+        webhook = Webhook.from_url('https://discord.com/api/webhooks/1238853077130412174/u7OGMX7WqdMdvSjAMyfGrwsZ4OQtDtwt4ExmZQnfOMV30p3Sxwk7vMUsg1pY2JDOFlGm', session=session)
         await webhook.send(msg, username=uname,silent=True)
 
 loop=asyncio.get_event_loop()
